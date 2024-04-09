57682acd1f16a249a390aa1220f87003b068e427
Gabriele <gabriele.palma.bool@gmail.com>
2024-04-09 10:00:46 +0200

    add

--- Front-end/src/components/Contacts.vue
+++ Front-end/src/components/Contacts.vue
@@ -14,10 +14,10 @@ export default {
     getRequest() {
       console.log(
         "PushTask:" +
-        store.NameSurname +
-        store.Email +
-        store.Title +
-        store.Message
+          store.NameSurname +
+          store.Email +
+          store.Title +
+          store.Message
       );
     },
   },
@@ -28,7 +28,10 @@ export default {
   <section>
     <div class="container contact-form">
       <div class="contact-image">
-        <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact" />
+        <img
+          src="https://image.ibb.co/kUagtU/rocket_contact.png"
+          alt="rocket_contact"
+        />
       </div>
       <form method="post">
         <div class="d-flex justify-content-center">
@@ -37,22 +40,49 @@ export default {
         <div class="row justify-content-center">
           <div class="col-md-6">
             <div class="form-group mb-3">
-              <input type="text" name="txtName" class="form-control" placeholder="NOME *" value="" />
+              <input
+                type="text"
+                name="txtName"
+                class="form-control"
+                placeholder="NOME *"
+                value=""
+              />
             </div>
             <div class="form-group mb-3">
-              <input type="email" name="txtEmail" class="form-control" placeholder="COGNOME *" value="" />
+              <input
+                type="text"
+                name="txtSurname"
+                class="form-control"
+                placeholder="COGNOME *"
+                value=""
+              />
             </div>
             <div class="form-group mb-3">
-              <input type="text" name="txtPhone" class="form-control" placeholder="EMAIL *" value="" />
+              <input
+                type="email"
+                name="txtPhone"
+                class="form-control"
+                placeholder="EMAIL *"
+                value=""
+              />
             </div>
             <div class="col-md-12 mb-3">
               <div class="form-group">
-                <textarea name="txtMsg" class="form-control" placeholder="MESSAGGIO *"
-                  style="width: 100%; height: 150px"></textarea>
+                <textarea
+                  name="txtMsg"
+                  class="form-control"
+                  placeholder="MESSAGGIO *"
+                  style="width: 100%; height: 150px"
+                ></textarea>
               </div>
             </div>
             <div class="form-group d-flex justify-content-center mb-3">
-              <input type="submit" name="btnSubmit" class="btnContact" value="INVIA MESSAGGIO" />
+              <input
+                type="submit"
+                name="btnSubmit"
+                class="btnContact"
+                value="INVIA MESSAGGIO"
+              />
             </div>
           </div>
         </div>
